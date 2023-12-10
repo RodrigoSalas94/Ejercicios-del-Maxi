4292b037540d0cf04b70fb889a3e36215bb85ffe
Rodrigo Salas <126690932+RodrigoSalas94@users.noreply.github.com>
2023-12-10 14:24:59 -0300

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const usuarios = require('./usuarios.json')
+const bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.json())
 const PORT = process.env.PORT ?? 1234
 
 app.get('/usuarios', (req, res) => {
@@ -8,28 +10,45 @@ app.get('/usuarios', (req, res) => {
 })
 
 app.post('/usuarios', (req, res) => {
-  let body = ''
-  // escuchar el evento data
-  req.on('data', chunk => {
-    body += chunk.toString()
-  })
-  req.on('end', () => {
-    const data = JSON.parse(body)
-    res.status(201).json(data)
-  })
+  const { nombre, email } = req.body
+
+  const usuarioExistente = usuarios.find(usuario => usuario.email === email)
+
+  if (usuarioExistente) {
+    return res.status(400).json({ message: 'El usuario ya existe' })
+  }
+
+  usuarios.push({ nombre, email })
+  return res.status(201).json({ message: 'Usuario creado correctamente' })
 })
+
+app.put('/usuarios/:id', (req, res) => {
+  const { nombre, email } = req.body
+
+  const usuarioIndex = usuarios.findIndex(usuario => usuario.id === req.params.id)
+
+  if (usuarioIndex >= 0) {
+    usuarios[usuarioIndex].nombre = nombre
+    usuarios[usuarioIndex].email = email
+    return res.status(200).json({ message: 'Usuario modificado correctamente' })
+  }
+
+  return res.status(404).json({ message: 'Usuario no encontrado' })
+})
+
 app.delete('/usuarios/:id', (req, res) => {
   const { id } = req.params
-  const usuarioIndex = usuarios.findIndex(movie => movie.id === id)
+  const usuarioIndex = usuarios.findIndex(usuario => usuario.id === id)
 
   if (usuarioIndex === -1) {
-    return res.status(404).json({ message: 'Movie not found' })
+    return res.status(404).json({ message: 'Usuario no encontrado' })
   }
 
   usuarios.splice(usuarioIndex, 1)
 
-  return res.json({ message: 'Movie deleted' })
+  return res.json({ message: 'Usuario eliminado' })
 })
+
 app.listen(PORT, () => {
   console.log(`server listening on port http://localhost:${PORT}`)
 })
